e2df2d6d5e3a9e1c89d9b280f7e236d14cbd309e
Adrian <are@fi.uba.ar>
2025-03-24 01:40:29 -0300

    use signal library to catch SIGTERM

--- server/common/server.py
+++ server/common/server.py
@@ -1,5 +1,6 @@
 import socket
 import logging
+from signal import signal, SIGTERM
 
 
 class Server:
@@ -8,6 +9,10 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self.exit_program = False
+
+    def signal_exit(self, signum, frame):
+        self.exit_program = True
 
     def run(self):
         """
@@ -17,10 +22,8 @@ class Server:
         communication with a client. After client with communucation
         finishes, servers starts to accept new connections again
         """
-
-        # TODO: Modify this program to handle signal to graceful shutdown
-        # the server
-        while True:
+        signal(SIGTERM, self.signal_exit)
+        while not self.exit_program:
             client_sock = self.__accept_new_connection()
             self.__handle_client_connection(client_sock)
 
