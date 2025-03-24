346b3e8e0b7f7d492bbde9092dc2ce2169b93924
Adrian <are@fi.uba.ar>
2025-03-24 13:56:10 -0300

    add line to close the server socket on SIGTERM

--- server/common/server.py
+++ server/common/server.py
@@ -27,6 +27,8 @@ class Server:
             client_sock = self.__accept_new_connection()
             self.__handle_client_connection(client_sock)
 
+        self._server_socket.close()
+
     def __handle_client_connection(self, client_sock):
         """
         Read message from a specific client socket and closes the socket
