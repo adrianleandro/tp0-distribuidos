000655b6f745459668b8933a5aef0bad1150bfaf
Adrian <are@fi.uba.ar>
2025-03-24 17:57:51 -0300

    set server timeout

--- server/common/server.py
+++ server/common/server.py
@@ -9,6 +9,7 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._server_socket.settimeout(1)
         self._client_socket = None
         self.exit_program = False
 
