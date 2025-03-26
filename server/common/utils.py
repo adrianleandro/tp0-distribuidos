b279193f351137a2c34e3f4b1764b7b5584a1e28
Adrian <are@fi.uba.ar>
2025-03-25 21:37:28 -0300

    add decode function

--- server/common/utils.py
+++ server/common/utils.py
@@ -2,7 +2,6 @@ import csv
 import datetime
 import time
 
-
 """ Bets storage location. """
 STORAGE_FILEPATH = "./bets.csv"
 """ Simulated winner number in the lottery contest. """
@@ -23,6 +22,22 @@ class Bet:
         self.document = document
         self.birthdate = datetime.date.fromisoformat(birthdate)
         self.number = int(number)
+
+    @classmethod
+    def decode(cls, agency: str, message: bytes) -> 'Bet':
+        def read_field(msg: bytes, index: int) -> tuple[str, int]:
+            length = msg[index]
+            field = msg[index + 1:index + 1 + length].decode('utf-8')
+            return field, index + 1 + length
+
+        idx = 0
+        first_name, idx = read_field(message, idx)
+        last_name, idx = read_field(message, idx)
+        document, idx = read_field(message, idx)
+        birth_date, idx = read_field(message, idx)
+        number, idx = read_field(message, idx)
+
+        return Bet(agency, first_name, last_name, document, birth_date, number)
 
 """ Checks whether a bet won the prize or not. """
 def has_won(bet: Bet) -> bool:
@@ -49,3 +64,13 @@ def load_bets() -> list[Bet]:
         for row in reader:
             yield Bet(row[0], row[1], row[2], row[3], row[4], row[5])
 
+def test_decode():
+    message = bytes.fromhex('044A75616E05506572657A093132333435363738390A313939302D30352D3135023432')
+    test_bet = Bet('1', 'Juan', 'Perez', '123456789', '1990-05-15', '42')
+    decoded_bet = Bet.decode('1', message)
+    assert decoded_bet.agency == test_bet.agency
+    assert decoded_bet.first_name == test_bet.first_name
+    assert decoded_bet.last_name == test_bet.last_name
+    assert decoded_bet.document == test_bet.document
+    assert decoded_bet.birthdate == test_bet.birthdate
+    assert decoded_bet.number == test_bet.number
