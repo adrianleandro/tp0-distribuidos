501e3a60e9a4290e7df55b38b6a95fc36b2eb766
Adrian <are@fi.uba.ar>
2025-03-26 17:07:20 -0300

    remove debug logging

--- server/common/server.py
+++ server/common/server.py
@@ -68,9 +68,7 @@ class Server:
         if chr(msg[0]) != 'b':
             raise ValueError('Bad message')
         agency_length = msg[1]
-        logging.debug(f'action: read | result: success | agency_length: {agency_length}')
         agency = msg[2:2+agency_length]
-        logging.debug(f'action: read | result: success | agency: {agency}')
         bet = Bet.decode(agency, msg[2+agency_length:])
         return bet
 
